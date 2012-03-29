7e460af1154b00d477a24dbe3888a7f4accf3b1c
Sascha Koehler <skoehler@pixeltricks.de>
2012-03-29 13:16:25 +0200

    Created repo

---
+++ code/SilvercartPaymentCOD.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Copyright 2012 pixeltricks GmbH
+ *
+ * This file is part of SilverCart.
+ *
+ * SilverCart is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SilverCart is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SilverCart.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Silvercart
+ * @subpackage Payment
+ */
+
+/**
+ * Class description.
+ *
+ * @package Silvercart
+ * @subpackage Payment
+ * @copyright 2012 pixeltricks GmbH
+ * @author Sascha Koehler <skoehler@pixeltricks.de>
+ * @since 29.03.2012
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
+ */
+class SilvercartPaymentCOD extends SilvercartPaymentMethod {
+ 
+    /**
+     * contains module name for display in the admin backend
+     *
+     * @var string
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 29.03.2012
+     */
+    protected $moduleName = 'COD';
+ 
+    /**
+     * Indicates whether a payment module has multiple payment channels or not.
+     *
+     * @var bool
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 29.03.2012
+     */
+    public static $has_multiple_payment_channels = false;
+ 
+    /**
+     * A list of possible payment channels.
+     *
+     * @var array
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 29.03.2012
+     */
+    public static $possible_payment_channels = array();
+ 
+    /**
+     * returns CMS fields
+     *
+     * @param mixed $params optional
+     *
+     * @return FieldSet
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 29.03.2012
+     */
+    public function getCMSFields($params = null) {
+        $fields         = parent::getCMSFieldsForModules($params);
+        $fieldLabels    = self::$field_labels;
+ 
+        return $fields;
+    }
+ 
+    // ------------------------------------------------------------------------
+    // processing methods
+    // ------------------------------------------------------------------------
+ 
+    /**
+     * hook to be called before order creation
+     *
+     * You can use this method to create tokens and/or jump to your payment
+     * provider.
+     *
+     * @return void
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 29.03.2012
+     */
+    public function processPaymentBeforeOrder() {
+    }
+ 
+    /**
+     * hook to be called after jumpback from payment provider; called before
+     * order creation
+     *
+     * @return void
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 29.03.2012
+     */
+    public function processReturnJumpFromPaymentProvider() {
+    }
+ 
+    /**
+     * hook to be called after order creation
+     *
+     * @param Order $orderObj object to be processed
+     *
+     * @return void
+     *
+     * @author Sascha Koehler <skoehler@pixeltricks.de>
+     * @since 29.03.2012
+     */
+    public function processPaymentAfterOrder($orderObj = array()) {
+    }
+ 
+    // ------------------------------------------------------------------------
+    // payment module specific methods
+    // ------------------------------------------------------------------------
+ 
+    // ...
+}
+?>
+
---
+++ lang/de_DE.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright 2012 pixeltricks GmbH
+ *
+ * This file is part of SilvercartPrepaymentPayment.
+ *
+ * SilvercartPaypalPayment is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SilvercartPrepaymentPayment is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with SilvercartPrepaymentPayment.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * German (Germany) language pack
+ *
+ * @package Silvercart
+ * @subpackage i18n
+ * @ignore
+ */
+
+i18n::include_locale_file('silvercart', 'en_US');
+
+if (array_key_exists('de_DE', $lang) && is_array($lang['de_DE'])) {
+    $lang['de_DE'] = array_merge($lang['en_US'], $lang['de_DE']);
+} else {
+    $lang['de_DE'] = $lang['en_US'];
+}
+
+$lang['de_DE']['SilvercartPaymentCOD']['NAME']  = 'Nachnahme';
+$lang['de_DE']['SilvercartPaymentCOD']['TITLE'] = 'Nachnahme';
+
